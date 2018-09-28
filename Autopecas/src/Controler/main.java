420434dc2520443ab3383ba4d317ec4240b54c1c
Aleska <Luiz Guilherme@DESKTOP-D6G4LD3>
2018-09-28 14:48:42 -0300

    Tela principal e script adicionados

---
+++ Autopecas/src/Controler/main.java
@@ -0,0 +1,16 @@
+
+package Controler;
+
+
+import View.TelaInicial;
+import View.TelaLogin;
+
+
+    public class main{
+        public static void main(String Args[]){
+            TelaLogin Telalogin = new TelaLogin();
+            Telalogin.show();
+            
+        }
+    }
+
